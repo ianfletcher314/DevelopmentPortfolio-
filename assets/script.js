990c8da464c030bd811ce0080e9f33b02aec45c5
Ian Fletcher <ianfletcher314@gmail.com>
2021-05-20 14:23:11 -0500

    event listeners added for all imgs to make them buttons. currently they console log working. next steps are making github pages for etb and mrf so i can link the event listeners. I also need to get new screenshots to put into the project buttons and then link the buttons to the deployed sites.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,31 @@
 // make event listener for client buttons 
+console.log("styleworking")
+
+const redirectEtb = () => {
+    console.log("etbworking")
+}
+const redirectMrf = () => {
+    console.log("mrfworking")
+}
+const redirectP2 = () => {
+    console.log("P1working")
+}
+const redirectP1 = () => {
+    console.log("P2working")
+}
+const redirectP3 = () => {
+    console.log("P3working")
+}
+
+
+
+
+
+
+
+
+document.getElementById("etblink").addEventListener("click", redirectEtb)
+document.getElementById("mrflink").addEventListener("click", redirectMrf)
+document.getElementById("project3").addEventListener("click", redirectP1)
+document.getElementById("project2").addEventListener("click", redirectP2)
+document.getElementById("project1").addEventListener("click", redirectP3)
